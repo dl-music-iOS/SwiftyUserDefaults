396f046b236cd72f4da62a9024d223dd3642f7d2
junho.lee <junho.lee2@linecorp.com>
2025-11-18 21:22:28 +0900

    Package.swift  -> Swift 5

--- Package.swift
+++ Package.swift
@@ -1,8 +1,24 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "SwiftyUserDefaults",
-    dependencies: [],
-    exclude: ["Sources/Info.plist", "Sources/SwiftyUserDefaults.h"]
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v4)
+    ],
+    products: [
+        .library(
+            name: "SwiftyUserDefaults",
+            targets: ["SwiftyUserDefaults"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftyUserDefaults",
+            path: "Sources",
+            exclude: ["Info.plist", "SwiftyUserDefaults.h"])
+    ],
+    swiftLanguageVersions: [.v5]
 )
-
